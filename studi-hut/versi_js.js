81cf3e63043377aa57f37aa998d896824ec866a7
AN-maz <andrianian398@gmail.com>
2025-10-03 12:44:14 +0700

    menambahkan fitur filter menu di nav

--- studi-hut/versi_js.js
+++ studi-hut/versi_js.js
@@ -1,15 +1,46 @@
+const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
 fetch('data/pizza.json')
     .then(response => response.json())
     .then(data => {
-        const menu = data.menu;
-        let cards = '';
+        const allMenu = data.menu;
+        displayMenu(allMenu);
 
-        menu.forEach(m => {
+        navLinks.forEach(link => {
+            link.addEventListener('click', function (e) {
+                e.preventDefault();
+                navLinks.forEach(nav => nav.classList.remove('active'));
+                this.classList.add('active');
 
-            cards += `
+                const kategori = this.innerHTML;
+                document.querySelector('.col h1').innerHTML = kategori === 'Home' ? 'All Menu' : kategori;
+
+                if (kategori === 'Home') {
+                    displayMenu(allMenu);
+                } else {
+                    const filteredMenu = allMenu.filter(
+                        m => m.kategori.toLowerCase() === kategori.toLowerCase()
+                    );
+                    displayMenu(filteredMenu);
+                }
+            });
+        });
+    })
+    .catch(err => {
+        console.error('Tidak bisa mengambil data:', err);
+    });
+
+
+function displayMenu(menu) {
+    const menuContainer = document.querySelector('#daftar-menu');
+    let cards = '';
+
+    menu.forEach(m => {
+
+        cards += `
             <div class="col-md-4">
                 <div class="card">
-                    <img src="img/pizza/${m.gambar}" class="card-img-top" alt="${m.gambar}">
+                    <img src="img/pizza/${m.gambar}" class="card-img-top" alt="${m.nama}">
                     <div class="card-body">
                         <h5 class="card-title">${m.nama}</h5>
                         <p class="card-text">${m.deskripsi}</p>
@@ -19,11 +50,7 @@ fetch('data/pizza.json')
                 </div>
             </div>
             `
-        });
-
-        const menuContainer = document.querySelector('#daftar-menu');
-        menuContainer.innerHTML = cards;
-    })
-    .catch(err => {
-        console.error('Tidak bisa mengambil data:', err);
     });
+
+    menuContainer.innerHTML = cards;
+}
